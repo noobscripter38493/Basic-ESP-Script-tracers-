37f2ac133f18c47b3704296ee5603a3854129fc2
noobscripter38493 <106062709+noobscripter38493@users.noreply.github.com>
2022-07-05 15:04:33 -0400

    Update and rename Tracer ESP.lua to ESP.lua

---
+++ ESP.lua
@@ -0,0 +1,108 @@
+local camera = workspace.CurrentCamera
+local viewportSize = camera.ViewportSize
+local worldToViewport = camera.WorldToViewportPoint
+
+local ESP = {
+    Enabled = true,
+    Objects = {}
+}
+
+function ESP:DrawAddon(class, properties)
+    local obj = Drawing.new(class)
+
+    for i, v in next, properties do
+        obj[i] = v
+    end
+
+    return obj
+end
+
+function ESP:New(part)
+    local obj = {}
+    obj.addons = {}
+    local addons = obj.addons
+
+    addons.Text = self:DrawAddon("Text", {
+        Text = part.Name,
+        Size = 20,
+        Center = true,
+        Outline = true,
+        Color = part.Color,
+        Visible = true
+    })
+
+    addons.Line = self:DrawAddon("Line", {
+        From = viewportSize / 2,
+        Thickness = 2.5,
+        Visible = true,
+        Color = part.Color
+    })
+
+    setmetatable(obj, {
+        __newindex = function(self, i, v)
+            for i2, v2 in next, addons do
+                if i2 == "Line" then
+                    v2.To = v
+                    continue
+                end
+
+                v2.Position = v 
+            end
+        end
+    })
+
+    self.Objects[part] = obj
+    
+    ESP:InitCheckParent(part)
+end
+
+function ESP:RemoveObj(part)
+    for _, obj in next, self.Objects[part].addons do
+        obj:Remove()
+    end
+
+    self.Objects[part] = nil
+end
+
+function ESP:InitCheckParent(part)
+    local parent_check; parent_check = part:GetPropertyChangedSignal("Parent"):Connect(function()
+        if part.Parent then return end
+        
+        parent_check:Disconnect()
+        self:RemoveObj(part)
+    end)
+end
+
+function ESP:SetVisible(obj, onscreen)
+    for _, v in next, obj.addons do
+        if not self.Enabled then
+            v.Visible = false    
+        end
+        
+        v.Visible = onscreen
+    end
+end
+
+function ESP:GetPosition(part, obj)
+    local v3, onscreen = worldToViewport(camera, part.Position)
+    
+    self:SetVisible(obj, onscreen)
+
+    local v2 = Vector2.new(v3.X, v3.Y)
+
+    return v2
+end
+
+function ESP:Update(part, obj)
+    local position = self:GetPosition(part, obj)
+    
+    obj.Position = position
+end
+
+game.RunService.RenderStepped:Connect(function()
+    for part, v in next, ESP.Objects do
+        ESP:Update(part, v)
+    end
+end)
+
+return ESP
